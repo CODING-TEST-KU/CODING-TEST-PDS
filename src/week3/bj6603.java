61aad30d09366870948223c6c9fdec2a72e79b40
dasol <pds2023@gmail.com>
2025-07-19 23:37:42 +0900

    bj6603

--- src/week3/bj6603.java
+++ src/week3/bj6603.java
@@ -3,32 +3,48 @@ package src.week3;
 import java.util.*;
 import java.io.*;
 
-
 public class bj6603 {
-    public static void main(String[] args) throws IOException{
 
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st=new StringTokenizer(br.readLine());
+    public static int n;
+    public static int[] lotto;
+    public static boolean[] visited;
+    public static List<Integer> numList;
 
-        int n =Integer.parseInt(st.nextToken());
+    public static void main(String[] args) throws IOException {
 
-        Set<Integer> numSet;
-        List<Integer> numList;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        while(n!=0){
-            numSet=new HashSet<>();
-            for(int i=0;i<n;i++){
-                numSet.add(Integer.parseInt(st.nextToken()));
+        n = Integer.parseInt(st.nextToken());
+        lotto = new int[6];
+        visited = new boolean[n];
+
+        while (n != 0) {
+            numList = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                numList.add(Integer.parseInt(st.nextToken()));
             }
-            numList =numSet.stream().toList();
 
-            Collections.sort(numList);
+            dfs(0,0);
 
-            st=new StringTokenizer(br.readLine());
-            n=Integer.parseInt(st.nextToken());
-
+            st = new StringTokenizer(br.readLine());
+            n = Integer.parseInt(st.nextToken());
+            System.out.println();
         }
     }
 
-
+    public static void dfs(int depth, int start) {
+        if (depth == 6) {
+            for (int num : lotto) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+            return;
+        } else if (depth < 6) {
+            for (int k = start; k < n; k++) {
+                    lotto[depth] = numList.get(k);
+                    dfs(depth+1,k+1);
+            }
+        }
+    }
 }
