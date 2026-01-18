7a24fd0fae01feb94970fc46fabfb139ae11a898
dasol <pds2023@gmail.com>
2026-01-18 17:06:15 +0900

    bj27448

--- src/algorithm_theme/implementation/bj27448.py
+++ src/algorithm_theme/implementation/bj27448.py
@@ -30,7 +30,8 @@
 
 
 import sys
-MAX=100000000
+
+MAX = 100000000
 input = sys.stdin.readline
 
 n, m, f = map(int, input().split())
@@ -40,7 +41,7 @@ wood = [list(input().rstrip()) for _ in range(n)]
 # 가로선 추출
 row_line = []
 for i in range(n - 1):
-    a=[]
+    a = []
     for j in range(m):
         if wood[i][j] != wood[i + 1][j]:
             a.append(1)
@@ -53,7 +54,7 @@ for i in range(n - 1):
 # 세로선 추출
 col_line = []
 for j in range(m - 1):
-    a=[]
+    a = []
     for i in range(n):
         if wood[i][j] != wood[i][j + 1]:
             a.append(1)
@@ -63,36 +64,53 @@ for j in range(m - 1):
             a.append(0)
     col_line.append(a)
 
-# 반직선 가능한지 확인하고 최소값 반환하는 함수
-def labor(line):
-    # 반직선 불가능
-    if 2 in line :
-        return line.count(1)
-    else:
-        return f
+#
+# # 반직선 가능한지 확인하고 최소값 반환하는 함수
+# def labor(line):
+#     # 반직선 불가능
+#     if 2 in line:
+#         return line.count(1)
+#     else:
+#         return f
 
 
 # 선 한 줄만 가져왔을 때
 def scan_line(aline):
-    if 1 not in aline:
-        return 0
+    L = len(aline)
+    left = 0
+    right = L - 1
 
-    line_min=MAX
-    for i in range(len(aline)):
+    # 왼쪽 끝에서 첫 2 까지의 1개수
+    left1 = 0
+    while left < L and aline[left] != 2:
+        if aline[left] == 1:
+            left1 += 1
+        left += 1
+
+    # 오른쪽 끝에서 첫 2 까지의 1개수
+    right1 = 0
+    while right >= 0 and aline[right] != 2 and right >= left:
+        if aline[right] == 1:
+            right1 += 1
+        right -= 1
+
+    # 양쪽이 2로 막힌 구간에서 1의 개수
+    mid1 = 0
+    for i in range(left, right + 1):
         if aline[i] == 1:
-            # 양쪽 다보기
-            dot_sum = labor(aline[i:]) + labor(aline[:i])
-            line_min=min(line_min,dot_sum)
-    return line_min
+            mid1 += 1
 
-ans=0
+    return min(f, left1) + min(f, right1) + mid1
+
+
+ans = 0
 
 # 가로선 전부
 for l in row_line:
-    ans+=scan_line(l)
+    ans += scan_line(l)
 
 # 세로선 전부
 for l in col_line:
-    ans+=scan_line(l)
+    ans += scan_line(l)
 
 print(ans)
