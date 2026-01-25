aaaadc21e62c3c3ad836ba10325474bc263466b9
dasol <pds2023@gmail.com>
2026-01-25 13:05:51 +0900

    bj16236_3

--- src/algorithm_theme/bj16236_3.py
+++ src/algorithm_theme/bj16236_3.py
@@ -1,63 +1,64 @@
+import sys
 from collections import deque
 
+input = sys.stdin.readline
+
 n = int(input())
-board = []
+
+sea = [list(map(int, input().split())) for _ in range(n)]
+
+baby_shark_size = 2
+shark_bally = 0
+ans = 0
+
+# 아기상어 위치 찾기
 for i in range(n):
-    row = list(map(int, input().split()))
     for j in range(n):
-        if row[j] == 9:
-            sx, sy = i, j
-            row[j] = 0
-    board.append(row)
+        if sea[i][j] == 9:
+            baby_r, baby_c = i, j
+            sea[i][j] = 0
 
-dx = [-1, 0, 0, 1]
-dy = [0, -1, 1, 0]
+# 탐색 큐
+q = deque()
 
-size = 2
-eat = 0
-time = 0
+# 상하좌우
+dr = [0, -1, 0, 1]
+dc = [-1, 0, 1, 0]
 
-def bfs(x, y, size):
-    visited = [[False]*n for _ in range(n)]
+
+def find_food():
+    global baby_shark_size, shark_bally, ans
     q = deque()
-    q.append((x, y, 0))
-    visited[x][y] = True
-
-    fishes = []
-    min_dist = float('inf')
-
+    q.append((baby_r, baby_c, 0))
+    visited[baby_r][baby_c] = True
     while q:
-        cx, cy, dist = q.popleft()
+        now_r, now_c, dist = q.popleft()
+        for i in range(4):
+            next_r, next_c = now_r + dr[i], now_c + dc[i]
+            if 0 <= next_r < n and 0 <= next_c < n:
+                if not visited[next_r][next_c] and sea[next_r][next_c] <= baby_shark_size:
+                    visited[next_r][next_c] = True
+                    q.append((next_r, next_c, dist + 1))
+                    # 상어보다 크기가 작으면 food에 넣기
+                    if 0 < sea[next_r][next_c] < baby_shark_size:
+                        food.append((next_r, next_c, dist+1))
 
-        if dist > min_dist:
-            break
-
-        for d in range(4):
-            nx, ny = cx + dx[d], cy + dy[d]
-            if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny]:
-                if board[nx][ny] <= size:
-                    visited[nx][ny] = True
-                    if 0 < board[nx][ny] < size:
-                        fishes.append((dist + 1, nx, ny))
-                        min_dist = dist + 1
-                    q.append((nx, ny, dist + 1))
-    return fishes
 
 while True:
-    fishes = bfs(sx, sy, size)
-    if not fishes:
+    # food, visited 초기화
+    food = []
+    visited = [[False] * n for _ in range(n)]
+    find_food()
+
+    if not food:
+        print(ans)
         break
-
-    fishes.sort()
-    dist, nx, ny = fishes[0]
-
-    time += dist
-    sx, sy = nx, ny
-    board[nx][ny] = 0
-    eat += 1
-
-    if eat == size:
-        size += 1
-        eat = 0
-
-print(time)
+    food.sort(key=lambda x:(x[2], x[0], x[1]))
+    eat = food[0]
+    baby_r, baby_c = eat[0], eat[1]
+    sea[eat[0]][eat[1]] = 0
+    shark_bally = shark_bally + 1
+    ans = ans + eat[2]
+    if shark_bally == baby_shark_size:
+        shark_bally = 0
+        baby_shark_size = baby_shark_size + 1
